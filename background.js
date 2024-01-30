0e2cb07d0543377bfc9f25122beef18b1faef494
Yousif Yacoub <yacoub.y@icloud.com>
2024-01-30 19:50:32 +0100

    WIP base structure

---
+++ background.js
@@ -0,0 +1,30 @@
+async function getUserEmail() {
+    const identity = await chrome.identity.getProfileUserInfo();
+    return identity.email;
+}
+
+
+chrome.runtime.onInstalled.addListener(() => {
+ chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    if (tabs[0].url?.startsWith("chrome://")) return undefined;
+    chrome.scripting.executeScript({
+      target: {tabId: tabs[0].id},
+      files: ['test.js']
+    });
+ });
+});
+
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (tab.url?.startsWith("chrome://")) return undefined;
+    if (changeInfo.url) {
+        const email = await getUserEmail();
+        try {
+            setTimeout(async () => {
+                await chrome.tabs.sendMessage(tabId, {message: "urlChanged", url: changeInfo.url, email: email});
+            }, 1000);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+});
--- server/server.js
+++ server/server.js
@@ -1,40 +1,82 @@
 const express = require('express');
 const cors = require('cors')
 const axios = require('axios');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 
+const dbUri = 'mongodb://localhost:27017';
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+    const timestamp = new Date().toLocaleString();
+    const method = req.method;
+    const url = req.url;
+    console.log(`[${timestamp}] ${method} ${url}`);
+    next();
+}
 // middleware
 app.use(cors())
+app.use(logger)
+app.use(express.json());
+
+
+const client = new MongoClient(dbUri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const connectDb = async () => {
+    try {
+        await client.connect();
+        await client.db("users").command({ping: 1});
+        console.log("You successfully connected to MongoDB!");
+    } catch (error) {
+        console.log("Could not connect to DB.")
+        console.error(error);
+        await client.close();
+    }
+}
 
 // routes
-app.get('/breach/:email', async (req, res) => {
-    const email = req.params?.email;
-    const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${encodeURIComponent(email)}`;
-    const headers = {
-        headers: {
-            'hibp-api-key': '70b84be5249249daa49a3afc49616a20',
-        }
-    }
-    res.setHeader('Content-Type', 'application/json');
-
-    try {
-        const response = await axios.get(url, headers);
-        if (response.status === 404) {
-            return res.send({"accounts": []});
-        } else {
-            return res.send({"accounts": response.data});
-        }
-    } catch (error) {
-        if (error.response.statusCode === 404) {
-            return res.send({"accounts": []});
-        }
-    }
-    // todo: store information
+app.post('/user', async (req, res) => {
+    // const collection = client.db("users").collection("users");
+    const userEmail = req.body.email;
+    const userUrl = req.body.url;
+    console.log(`User email: ${userEmail}, User URL: ${userUrl}`);
+    // const url = `https://haveibeenpwned.com/api/v3/breachedaccount/${encodeURIComponent(email)}`;
+    // const headers = {
+    //     headers: {
+    //         'hibp-api-key': '70b84be5249249daa49a3afc49616a20',
+    //     }
+    // }
+    // res.setHeader('Content-Type', 'application/json');
+    // let compromised = false;
+    // let accounts = [];
+    //
+    // try {
+    //     const response = await axios.get(url, headers);
+    //     if (response.status !== 404) {
+    //         accounts = response.data;
+    //         compromised = true;
+    //     }
+    //     return res.send({"accounts": accounts});
+    // } catch (error) {
+    //     if (error.response.statusCode === 404) {
+    //         return res.send({"accounts": accounts});
+    //     }
+    // }
+    // // todo: store information
+    // console.log("Writing into DB...");
+    // await collection.insertOne({email: email, compromised: compromised, accounts: accounts});
+    // console.log("Done!");
+    return res.send({"userEmail": userEmail, "userUrl": userUrl});
 });
 
 // Start the server and listen on the specified port
-app.listen(port, () => {
+app.listen(port, async () => {
+    // await connectDb();
     console.log(`Server is running on http://localhost:${port}`);
 });
