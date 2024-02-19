5c8e8ba9c89f1b3ade1b73456063c38e10c07622
Yousif Yacoub <yacoub.y@icloud.com>
2024-02-19 20:43:11 +0100

    Add survey

--- worker.js
+++ worker.js
@@ -1,11 +1,12 @@
-const serverURL = 'https://opportune-moments-server-c68b7d59b461.herokuapp.com';
+// const serverURL = 'https://opportune-moments-server-c68b7d59b461.herokuapp.com';
+const serverURL = 'http://localhost:3000';
 
-async function taskRequest(userEmail, userUrl) {
+async function requestPopup(userEmail, userUrl) {
     const body = {
         email: userEmail,
         url: userUrl,
     }
-    const url = `${serverURL}/task`;
+    const url = `${serverURL}/popup`;
 
     return fetch(url, {
         method: 'POST',
@@ -16,66 +17,99 @@ async function taskRequest(userEmail, userUrl) {
     });
 }
 
+const surveyQuestion = async (title, text) => {
+    return swal({
+        title: title,
+        text: text,
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        content: "input",
+    }).then((value) => {
+        if (!value) {
+            swal({text: "Please answer the question.", closeOnEsc: false, closeOnClickOutside: false}).then(() => {
+                surveyQuestion(title, text);
+            });
+        }
+        return value;
+    });
+}
+
 chrome.runtime.onMessage.addListener(async (request, sender, _sendResponse) => {
-    if (request.message === "urlChanged") {
-        const res = await taskRequest(request.email, request.url);
-        const data = await res.json();
-        const taskType = data.type;
-        if (!data) {
-            return;
-        }
-        const instructionsRes = await fetch(`${serverURL}/instructions/${data.type}/${data.domain}`);
-        const instructions = await instructionsRes.json();
-        let headline = "";
-        let text = "";
-        let affirmative = false;
-        if (data.type === "2fa") {
-            headline = "This website offers 2FA.";
-            text = "Would you like to enable it?";
-        } else if (data.type === "pw") {
-            headline = "Compromised password detected!";
-            text = `Your password for ${data.service} has been found in a data breach. Would you like to change it?`;
-        }
-        swal({
-            title: headline,
-            text: text,
-            icon: "warning",
-            buttons: {
-                cancel: "No, thanks",
-                ok: true,
-            },
-        }).then((value) => {
-            if (value === "ok") {
-                affirmative = true;
-                return swal({
-                    title: "Awesome! Here's how:",
-                    text: instructions.data,
-                    icon: "info",
-                });
+        if (request.message === "urlChanged") {
+            const res = await requestPopup(request.email, request.url);
+            const data = await res.json();
+            if (!data) {
+                return;
             }
-        }).then(() => {
-            if (affirmative) {
-                return swal("Why was this a good moment?", {
-                    content: "input",
-                });
+            const isSurvey = data.survey || false;
+            if (isSurvey) {
+                let feedback = {};
+                return surveyQuestion("Question 1", "Text 1")
+                    .then(
+                        (value) => {
+                            feedback["question1"] = value;
+                            return surveyQuestion("Question 2", "Text 2");
+                        }
+                    )
+                    .then(
+                        (value) => {
+                            feedback["question2"] = value;
+                            return surveyQuestion("Question 3", "Text 3");
+                        }
+                    )
+                    .then(
+                        (value) => {
+                            feedback["question3"] = value;
+                            return swal({
+                                title: "Thank you!",
+                                text: `Question 1: ${feedback.question1}\nQuestion 2: ${feedback.question2}\nQuestion 3: ${feedback.question3}`,
+                                icon: "success",
+                            });
+                        }
+                    )
             }
-            return swal("Why is this an inappropriate moment?", {
-                content: "input",
-            });
-        }).then(async (feedback) => {
-            await fetch(`${serverURL}/feedback`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
+
+            const taskType = data.type;
+            const instructionsRes = await fetch(`${serverURL}/instructions/${data.type}/${data.domain}`);
+            const instructions = await instructionsRes.json();
+            let headline = "";
+            let text = "";
+            if (data.type === "2fa") {
+                headline = "This website offers 2FA.";
+                text = "Would you like to enable it?";
+            } else if (data.type === "pw") {
+                headline = "Compromised password detected!";
+                text = `Your password for ${data.service} has been found in a data breach. Would you like to change it?`;
+            }
+            swal({
+                title: headline,
+                text: text,
+                icon: "warning",
+                buttons: {
+                    cancel: "No, thanks",
+                    ok: true,
                 },
-                body: JSON.stringify({
-                    email: request.email,
-                    domain: data.domain,
-                    affirmative: affirmative,
-                    feedback: feedback,
-                    taskType: taskType,
-                }),
+            }).then((value) => {
+                if (value === "ok") {
+                    return swal({
+                        title: "Awesome! Here's how:",
+                        text: instructions.data,
+                        icon: "info",
+                    });
+                }
+            }).then(async () => {
+                await fetch(`${serverURL}/interaction`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        email: request.email,
+                        domain: data.domain,
+                        taskType: taskType,
+                    }),
+                });
             });
-        });
+        }
     }
-});
+);
