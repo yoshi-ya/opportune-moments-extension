3c17028b0ccda880bc8e6335a571220713b8d9cc
Yousif Yacoub <yacoub.y@icloud.com>
2024-02-27 15:50:29 +0100

    Refactor for better readability

--- worker.js
+++ worker.js
@@ -50,6 +50,113 @@ const surveyQuestion = async (title, text) => {
     });
 };
 
+const nonAffirmativeSurvey = async (title, email, domain, taskType, survey) => {
+    return surveyQuestion(title, "Why did you not do the task?")
+        .then(
+            (value) => {
+                survey["reason"] = value;
+                return fetch(`${serverURL}/survey`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email,
+                        domain,
+                        survey,
+                        taskType,
+                    }),
+                });
+            }
+        ).then(() => {
+            return swal({
+                title: "Thank you for your time!",
+                icon: "success",
+            });
+        });
+};
+
+const affirmativeSurvey = async (title, email, domain, taskType, survey) => {
+    return surveyQuestion(title, "From which location did you do the task?")
+        .then(
+            (value) => {
+                survey["location"] = value;
+                return surveyQuestion(title, "What were you doing before engaging with the task?");
+            }
+        )
+        .then(
+            (value) => {
+                survey["context"] = value;
+                return surveyQuestion(title, "Why was that a good moment to do the task?");
+            }
+        )
+        .then(
+            (value) => {
+                survey["reason"] = value;
+                fetch(`${serverURL}/survey`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email,
+                        domain,
+                        survey,
+                        taskType,
+                    }),
+                }).then(() => {
+                    return swal({
+                        title: "Thank you for your time!",
+                        icon: "success",
+                    });
+                }).catch(() => {
+                    console.log("Could not send survey to server.");
+                });
+            });
+};
+
+const getInstructions = async (type, domain) => {
+    const res = await fetch(`${serverURL}/instructions/${type}/${domain}`);
+    const data = await res.json();
+    return data.data;
+};
+
+const securityTaskPopup = async (title, text, instructions, email, domain, taskType) => {
+    return swal({
+        title,
+        text,
+        icon: "warning",
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        buttons: {
+            cancel: "No, thanks",
+            ok: true,
+        },
+    }).then((value) => {
+        if (value === "ok") {
+            return swal({
+                title: "Awesome! Here's how:",
+                text: instructions,
+                closeOnClickOutside: false,
+                closeOnEsc: false,
+                icon: "info",
+            });
+        }
+    }).then(async () => {
+        await fetch(`${serverURL}/interaction`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                domain,
+                taskType,
+            }),
+        });
+    });
+};
+
 chrome.runtime.onMessage.addListener(async (request, sender, _sendResponse) => {
     if (request.message === "urlChanged") {
         const res = await requestPopup(request.email, request.url);
@@ -69,75 +176,15 @@ chrome.runtime.onMessage.addListener(async (request, sender, _sendResponse) => {
                 ? `Recently, you were tasked to enable 2FA for ${data.domain}. Did you do it?`
                 : `Recently, you were tasked to change your password for ${data.domain}. Did you do it?`;
             return surveyIntro("It's time for a super quick survey!", text)
-                .then((value) => {
+                .then(async (value) => {
                     if (value !== "yes") {
-                        return surveyQuestion(surveyTitle, "Why did you not do the task?")
-                            .then(
-                                (value) => {
-                                    survey["reason"] = value;
-                                    return fetch(`${serverURL}/survey`, {
-                                        method: "POST",
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                        },
-                                        body: JSON.stringify({
-                                            email: request.email,
-                                            domain: data.domain,
-                                            survey: survey,
-                                            taskType: data.type,
-                                        }),
-                                    });
-                                }
-                            ).then(() => {
-                                return swal({
-                                    title: "Thank you for your time!",
-                                    icon: "success",
-                                });
-                            });
+                        return await nonAffirmativeSurvey(surveyTitle, request.email, data.domain, data.type, survey);
                     } else {
-                        return surveyQuestion(surveyTitle, "From which location did you do the task?")
-                            .then(
-                                (value) => {
-                                    survey["location"] = value;
-                                    return surveyQuestion(surveyTitle, "What were you doing before engaging with the task?");
-                                }
-                            )
-                            .then(
-                                (value) => {
-                                    survey["context"] = value;
-                                    return surveyQuestion(surveyTitle, "Why was that a good moment to do the task?");
-                                }
-                            )
-                            .then(
-                                (value) => {
-                                    survey["reason"] = value;
-                                    fetch(`${serverURL}/survey`, {
-                                        method: "POST",
-                                        headers: {
-                                            "Content-Type": "application/json",
-                                        },
-                                        body: JSON.stringify({
-                                            email: request.email,
-                                            domain: data.domain,
-                                            survey: survey,
-                                            taskType: data.type,
-                                        }),
-                                    }).then(() => {
-                                        return swal({
-                                            title: "Thank you for your time!",
-                                            icon: "success",
-                                        });
-                                    }).catch(() => {
-                                        console.log("Could not send survey to server.");
-                                    });
-                                });
+                        return await affirmativeSurvey(surveyTitle, request.email, data.domain, data.type, survey);
                     }
                 });
         }
-
-        const taskType = data.type;
-        const instructionsRes = await fetch(`${serverURL}/instructions/${data.type}/${data.domain}`);
-        const instructions = await instructionsRes.json();
+        const instructions = await getInstructions(data.type, data.domain);
         let headline = "";
         let text = "";
         if (data.type === "2fa") {
@@ -147,35 +194,6 @@ chrome.runtime.onMessage.addListener(async (request, sender, _sendResponse) => {
             headline = "Compromised password detected!";
             text = `Your password for ${data.domain} has been found in a data breach. Would you like to change it?`;
         }
-        swal({
-            title: headline,
-            text: text,
-            icon: "warning",
-            buttons: {
-                cancel: "No, thanks",
-                ok: true,
-            },
-        }).then((value) => {
-            if (value === "ok") {
-                return swal({
-                    title: "Awesome! Here's how:",
-                    text: instructions.data,
-                    icon: "info",
-                });
-            }
-        }).then(async () => {
-            await fetch(`${serverURL}/interaction`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: request.email,
-                    domain: data.domain,
-                    taskType: taskType,
-                }),
-            });
-        });
+        await securityTaskPopup(headline, text, instructions, request.email, data.domain, data.type);
     }
-}
-);
+});
