4a79ce3b93e792e9b172577a523eda357a1c3133
Yousif Yacoub <yacoub.y@icloud.com>
2024-03-05 21:16:26 +0100

    Change icon

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ async function getUserEmail() {
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        if (tabs[0].url?.startsWith("chrome://")) return undefined;
+        if (tabs[0].url?.startsWith("chrome")) return undefined;
         chrome.scripting.executeScript({
             target: {tabId: tabs[0].id},
             files: ["worker.js"]
@@ -20,7 +20,7 @@ chrome.runtime.onInstalled.addListener(() => {
 
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if (tab.url?.startsWith("chrome://")) return undefined;
+    if (tab.url?.startsWith("chrome")) return undefined;
     if (changeInfo.url) {
         const email = await getUserEmail();
 
